77c59e09bb1d8fb4a7de33bebefa3e75c753a510
Daniël Knippers <daniel.knippers@perplex.nl>
2020-04-14 17:58:08 +0200

    CopyPaste: create new Nested Content keys for all descendant Nested Content items, not only the one of the block

--- src/Perplex.ContentBlocks/App_Plugins/Perplex.ContentBlocks/perplex.content-blocks.service.js
+++ src/Perplex.ContentBlocks/App_Plugins/Perplex.ContentBlocks/perplex.content-blocks.service.js
@@ -22,17 +22,53 @@
             return copy;
         }
 
+        /**
+         * Copies the given Content Block and all its data
+         * @param {any} original
+         * @returns {object} Copied Content Block
+         */
         function copyContentBlock(original) {
             var copy = angular.copy(original);
 
             // Always generate a new unique id
             copy.id = String.CreateGuid();
 
-            if (Array.isArray(copy.content) && copy.content[0] && copy.content[0].key) {
+            if (Array.isArray(copy.content)) {
                 // A Nested Content has its own unique id and caches an item based on it.
                 // When we have create a copy we should therefore also update their Nested Content
                 // id to prevent getting old cached values from Nested Content.
-                copy.content[0].key = String.CreateGuid();
+                copy.content.forEach(updateNestedContentKey);
+            }
+
+            /**
+             * Updates the key of this Nested Content and all its inner Nested Content items
+             * @param {object} nestedContent Nested Content item
+             */
+            function updateNestedContentKey(nestedContent) {
+                if (nestedContent == null || nestedContent.key == null) {
+                    return;
+                }
+
+                nestedContent.key = String.CreateGuid();
+
+                // Also update any nested Nested Content items inside this nestedContent
+                for (var property in nestedContent) {
+                    if (!nestedContent.hasOwnProperty(property)) {
+                        continue;
+                    }
+
+                    var value = nestedContent[property];
+                    if (!isNestedContentValue(value)) {
+                        continue;
+                    }
+                    
+                    // Update keys of all items
+                    value.forEach(updateNestedContentKey);                    
+                }
+
+                function isNestedContentValue(value) {
+                    return Array.isArray(value) && value.length > 0 && value[0].key != null && value[0].ncContentTypeAlias != null;
+                }
             }
 
             return copy;
