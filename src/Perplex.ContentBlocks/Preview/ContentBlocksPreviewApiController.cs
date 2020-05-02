1954d43ab775fb07db7607e3c3e42c656e657d33
Daniël Knippers <daniel.knippers@perplex.nl>
2020-05-02 20:58:29 +0200

    Include Jump.js until Chrome fixes smooth scrolling behavior with window.scrollTo

--- src/Perplex.ContentBlocks/Preview/ContentBlocksPreviewApiController.cs
+++ src/Perplex.ContentBlocks/Preview/ContentBlocksPreviewApiController.cs
@@ -102,35 +102,37 @@ namespace Perplex.ContentBlocks.Preview
             var body = doc.DocumentNode.SelectSingleNode("//body");
             if (body != null)
             {
-                var script = GetScrollSyncScript(doc);
-                body.AppendChild(script);
+                AppendScrollScript(body);
             }
 
             // .OuterHtml does not contain a DOCTYPE declaration so we add this manually
             return "<!DOCTYPE html>" + doc.DocumentNode.OuterHtml;
         }
 
-        private HtmlNode GetScrollSyncScript(HtmlDocument doc)
+        private void AppendScrollScript(HtmlNode parent)
         {
             string script = @"
+                <script>
                 window.addEventListener('message', receiveMessage, false);
 
                 function receiveMessage(event) {
                     var element = document.getElementById(event.data.blockId);
 
-                    if (element != null && typeof window.scrollTo === ""function"") {
-                        window.scrollTo({
-                            top: element.offsetTop,
-                            behavior: ""smooth""
-                        });
+                    if (element != null && typeof window.jump === ""function"") {
+                        window.jump(element, { duration: 500 });
                     }
-                }";
+                }
 
-            return new HtmlNode(HtmlNodeType.Element, doc, 0)
-            {
-                Name = "script",
-                InnerHtml = script
-            };
+                // Jump.js v1.0.2 - https://github.com/callmecavs/jump.js
+                // Used as a (temporary?) replacement for window.scrollTo.
+                // Chrome v81+ stopped respecting { behavior: ""smooth"" } and snaps instantly instead.
+                // When it has been fixed this library can be removed.
+                !function(){""use strict"";var o=function(o,n,t,i){return(o/=i/2)<1?t/2*o*o+n:-t/2*(--o*(o-2)-1)+n},n=""function""==typeof Symbol&&""symbol""==typeof Symbol.iterator?function(o){return typeof o}:function(o){return o&&""function""==typeof Symbol&&o.constructor===Symbol&&o!==Symbol.prototype?""symbol"":typeof o},t=function(){var t=void 0,i=void 0,e=void 0,r=void 0,u=void 0,c=void 0,d=void 0,a=void 0,f=void 0,s=void 0,l=void 0,v=void 0;function b(){return window.scrollY||window.pageYOffset}function w(o){return o.getBoundingClientRect().top+i}function y(o){f||(f=o),l=u(s=o-f,i,d,a),window.scrollTo(0,l),s<a?window.requestAnimationFrame(y):function(){window.scrollTo(0,i+d),t&&c&&(t.setAttribute(""tabindex"","" - 1""),t.focus());""function""==typeof v&&v();f=!1}()}return function(f){var s=arguments.length>1&&void 0!==arguments[1]?arguments[1]:{};switch(a=s.duration||1e3,r=s.offset||0,v=s.callback,u=s.easing||o,c=s.a11y||!1,i=b(),void 0===f?""undefined"":n(f)){case""number"":t=void 0,c=!1,e=i+f;break;case""object"":e=w(t=f);break;case""string"":t=document.querySelector(f),e=w(t)}switch(d=e-i+r,n(s.duration)){case""number"":a=s.duration;break;case""function"":a=s.duration(d)}window.requestAnimationFrame(y)}}();window.jump=t}();
+                </script>
+            ";
+
+            var scriptNode = HtmlNode.CreateNode(script);
+            parent.AppendChild(scriptNode);
         }
 
         private string GetPreviewPath(int pageId, string culture)
