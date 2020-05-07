2c329ded49979136bebe6824ed19bc53dfeb1b8c
Daniël Knippers <daniel.knippers@perplex.nl>
2020-05-07 09:55:47 +0200

    Do not create a data type by default, this can be done by users

--- src/Perplex.ContentBlocks/Migrations/Utils/IMigrationPlanExecuter.cs
+++
@@ -1,9 +0,0 @@
-﻿using Umbraco.Core.Migrations;
-
-namespace Perplex.ContentBlocks.Migrations.Utils
-{
-    public interface IMigrationPlanExecuter
-    {
-        void Execute(MigrationPlan plan);
-    }
-}
