b3c36353e5b5b9dc792b37862cc36ccea45c8883
Daniël Knippers <daniel.knippers@perplex.nl>
2020-05-21 21:28:15 +0200

    Added <content-blocks-icon> component
    Reduces repeating a bunch of code, e.g. the path to our icons.svg file

---
+++ src/Perplex.ContentBlocks/App_Plugins/Perplex.ContentBlocks/components/perplex.content-blocks.icon.js
@@ -0,0 +1,19 @@
+﻿angular.module("perplexContentBlocks").component("contentBlocksIcon", {
+    template:
+        '<svg class="l-icon" ng-class="[\'l-icon--\' + $ctrl.size, $ctrl.cssClass]">' +
+        '<use ng-attr-href="/App_Plugins/Perplex.ContentBlocks/assets/icons.svg#icon-{{$ctrl.icon}}"></use>' +
+        '</svg> ',
+    bindings: {
+        icon: "@",
+        size: "@?",
+        cssClass: "@?",
+    },
+    controller: function () {
+        this.$onInit = function () {
+            if (this.size == null) {
+                // Default to small
+                this.size = "sm";
+            }
+        }
+    }
+});
