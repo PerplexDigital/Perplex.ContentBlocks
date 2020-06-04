6b754cee37f2e98bec085c3eff762781caeec929
Daniël Knippers <daniel.knippers@perplex.nl>
2020-06-04 20:08:49 +0200

    Added Perplex.ContentBlocks.Core NuGet package that contains the DLL only.
    This means there will now be 2 NuGet packages:
    - Perplex.ContentBlocks
    - Perplex.ContentBlocks.Core
    
    These will always be released together, even though one of the packages may
    technically not have changed. Their versions will also be completely in sync, as
    there will still be just 1 version of Perplex.ContentBlocks, but of course
    either package needs to be versioned seperately for NuGet.

--- pkg/nuget/pack.ps1
+++ pkg/nuget/pack.ps1
@@ -2,55 +2,34 @@ Push-Location $PSScriptRoot
 
 Write-Host "Packing for NuGet ..."
 
-.\nuget pack "..\..\src\Perplex.ContentBlocks\Perplex.ContentBlocks.csproj" -p NoWarn=NU5128 -p Configuration=Release
-
 $version = ..\version.ps1
-$nugetFile = Get-ChildItem -Filter "*$version.nupkg"
-if(!$nugetFile) {
-    Throw "No NuGet file found!"
+
+$nuspecTokens = @{
+    id = "Perplex.ContentBlocks";
+    version = $version;
+    title = $id;
+    authors = "Perplex Digital";
+    owners = $authors;
+    description = "Block based content editor for Umbraco";
+    license = "MIT";
+    requireLicenseAcceptance = "false";
+    projectUrl = "https://github.com/PerplexDigital/Perplex.ContentBlocks";
+    copyright = "© Perplex Digital";
+    tags = "umbraco property editor content block";
+    releaseNotes = "https://github.com/PerplexDigital/Perplex.ContentBlocks/blob/master/RELEASE_NOTES.md";
+    repositoryUrl = "https://github.com/PerplexDigital/Perplex.ContentBlocks.git";
 }
 
-$tmpDir = "_tmp"
+$props = ($nuspecTokens.Keys | %{ "$_=$($nuspecTokens[$_])" }) -join ";"
 
-if(Test-Path $tmpDir) {
-    Remove-Item $tmpDir -Recurse
-}
-
-# Rename to .zip to use in Expand-Archive
-$nugetZip = "$nugetFile.zip"
-Rename-Item $nugetFile $nugetZip
-
-# Unzip
-Expand-Archive $nugetZip $tmpDir
-
-# Read .nuspec
-$nuspecFile = "$tmpDir\Perplex.ContentBlocks.nuspec"
-[xml]$nuspec = Get-Content $nuspecFile
-
-# Add <group targetFramework=".NETFramework4.7.2">
-$group = $nuspec.CreateElement("group", $nuspec.package.NamespaceURI)
-$group.SetAttribute("targetFramework", ".NETFramework4.7.2")
-
-$dependencies = $nuspec.package.metadata.dependencies
-
-# Add all <dependency> elements to <group
-$dependencies.dependency | % { $group.AppendChild($_) } | Out-Null
-$dependencies.AppendChild($group) | Out-Null
-
-$nuspec.Save("$pwd\$nuspecFile")
-
-# Update Zip
-Compress-Archive $nuspecFile $nugetZip -Update
-
-# Rename back to .nupkg
-Rename-Item $nugetZip $nugetFile
-
-# Remove temporary directory
-Remove-Item $tmpDir -Recurse
+# Generate .nupkgs
+.\nuget pack Perplex.ContentBlocks.nuspec -p "$props" -p NoWarn=NU5105,NU5128
+.\nuget pack Perplex.ContentBlocks.Core.nuspec -p "$props" -p NoWarn=NU5105
 
 Write-Host "Done!"
 
-# Write resulting .nupkg file to Pipeline
-Write-Output (Get-Item $nugetFile)
+# Write resulting .nupkgs file to pipeline
+$nugetFiles = Get-ChildItem -Filter "*$version.nupkg"
+Write-Output $nugetFiles
 
 Pop-Location
