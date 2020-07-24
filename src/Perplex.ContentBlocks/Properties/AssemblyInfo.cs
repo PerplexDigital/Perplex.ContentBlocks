aaa2b6043b28aec26e477ee13f83170f82644548
Daniël Knippers <daniel.knippers@perplex.nl>
2020-07-24 13:18:22 +0200

    Version 1.5.0

--- src/Perplex.ContentBlocks/Properties/AssemblyInfo.cs
+++ src/Perplex.ContentBlocks/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.5.0")]
-[assembly: AssemblyInformationalVersion("1.5.0-rc.1")]
+[assembly: AssemblyInformationalVersion("1.5.0")]
