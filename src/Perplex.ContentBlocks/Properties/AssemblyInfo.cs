694c2100be6ca40aacb1bfccb4000eb3a7fbec29
Daniël Knippers <daniel.knippers@perplex.nl>
2021-02-04 15:04:09 +0100

    Version 1.7.0-alpha.3

--- src/Perplex.ContentBlocks/Properties/AssemblyInfo.cs
+++ src/Perplex.ContentBlocks/Properties/AssemblyInfo.cs
@@ -32,4 +32,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.7.0")]
-[assembly: AssemblyInformationalVersion("1.7.0-alpha.2")]
+[assembly: AssemblyInformationalVersion("1.7.0-alpha.3")]
