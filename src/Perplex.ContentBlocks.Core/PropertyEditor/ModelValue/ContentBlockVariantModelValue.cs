1ee6b4534a1bc8f06c9adcc83f6408de19b2f77a
Daniël Knippers <daniel.knippers@perplex.nl>
2021-09-22 09:22:07 +0200

    Add variants to model

---
+++ src/Perplex.ContentBlocks.Core/PropertyEditor/ModelValue/ContentBlockVariantModelValue.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+
+namespace Perplex.ContentBlocks.PropertyEditor.ModelValue
+{
+    public class ContentBlockVariantModelValue
+    {
+        [JsonProperty("id")]
+        public Guid Id { get; set; }
+
+        [JsonProperty("variant")]
+        public string Variant { get; set; }
+
+        /// <summary>
+        /// JSON NestedContent
+        /// </summary>
+        [JsonProperty("content")]
+        public JArray Content { get; set; }
+    }
+}
