f1f657a2f37dde315ad92679542234138630c86b
Daniël Knippers <daniel.knippers@perplex.nl>
2021-09-24 10:57:13 +0200

    Add sample custom component

---
+++ src/DemoWebsite.v8/App_Plugins/MyContentBlocksAddon/component.js
@@ -0,0 +1,20 @@
+﻿angular.module("MyContentBlocksAddon").component("component", {
+    templateUrl: "/App_Plugins/MyContentBlocksAddon/component.html",
+
+    bindings: {
+        block: "<",
+        definition: "<",
+    },
+
+    require: {
+        blockCtrl: "^perplexContentBlock"
+    },
+
+    controller: [
+        function componentController() {
+            this.$onInit = function () {
+                // TODO
+            }
+        }
+    ],
+});
---
+++ src/DemoWebsite.v8/App_Plugins/MyContentBlocksAddon/init.js
@@ -0,0 +1,8 @@
+﻿angular.module("MyContentBlocksAddon", ["perplexContentBlocks"])
+angular.module("umbraco").requires.push("MyContentBlocksAddon");
+
+angular.module("MyContentBlocksAddon").run([
+    "perplexContentBlocksCustomComponents",
+    function (customComponents) {
+        customComponents.block.main = "component";
+    }]);
