bcd22c5fd5e8d7f6dc819b77cbde97ca3fd11cd4
Daniël Knippers <daniel.knippers@perplex.nl>
2021-09-27 14:56:17 +0200

    Add component to render legacy validation messages

---
+++ src/Perplex.ContentBlocks/App_Plugins/Perplex.ContentBlocks/components/perplex.content-blocks.legacy-validation-messages.js
@@ -0,0 +1,4 @@
+﻿angular.module("perplexContentBlocks").component("perplexContentBlocksLegacyValidationMessages", {
+    template: '<div ng-repeat="validationMessage in $ctrl.validationMessages track by validationMessage.property+validationMessage.errorMessage" style="display: flex;"><code ng-bind="validationMessage.property" ng-show="validationMessage.property" class="alert alert-info property-error"></code><div ng-bind="validationMessage.errorMessage" ng-show="validationMessage.errorMessage" class="alert alert-error property-error"></div></div>',
+    bindings: { validationMessages: "<" },
+});
