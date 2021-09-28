a8e6782f315d7b4640fca88d849ea36bac4b5f41
Daniël Knippers <daniel.knippers@perplex.nl>
2021-09-28 08:43:02 +0200

    Add some comments to legacy validation messages component

--- src/Perplex.ContentBlocks/App_Plugins/Perplex.ContentBlocks/components/perplex.content-blocks.legacy-validation-messages.js
+++ src/Perplex.ContentBlocks/App_Plugins/Perplex.ContentBlocks/components/perplex.content-blocks.legacy-validation-messages.js
@@ -1,4 +1,9 @@
-﻿angular.module("perplexContentBlocks").component("perplexContentBlocksLegacyValidationMessages", {
+﻿// Component to render validation messages in Umbraco < 8.7.
+// In Umbraco 8.7 validation messages were revamped and show up 
+// at each property including inside Nested Content, recursively. 
+// This is a great improvement and we use that in ContentBlocks too.
+// For older versions we render validation messages ourselves at each block with this component.
+angular.module("perplexContentBlocks").component("perplexContentBlocksLegacyValidationMessages", {
     template: '<div ng-repeat="validationMessage in $ctrl.validationMessages track by validationMessage.property+validationMessage.errorMessage" style="display: flex;"><code ng-bind="validationMessage.property" ng-show="validationMessage.property" class="alert alert-info property-error"></code><div ng-bind="validationMessage.errorMessage" ng-show="validationMessage.errorMessage" class="alert alert-error property-error"></div></div>',
     bindings: { validationMessages: "<" },
 });
