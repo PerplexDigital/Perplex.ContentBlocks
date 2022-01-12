ee3223b4c0f1b72810086a83b40356f28f322999
Daniël Knippers <daniel.knippers@perplex.nl>
2022-01-12 11:38:16 +0100

    Use dataTypeResource.getById instead of .getAll to fetch by key
    This is possible since 8.6.0, before that there was never a GetById(Guid)
    on DataTypeController. Since ContentBlocks now has a dependency on Umbraco
    8.7+ we can use this.
    https://github.com/umbraco/Umbraco-CMS/pull/6847

--- src/Perplex.ContentBlocks/App_Plugins/Perplex.ContentBlocks/utils/property/perplex.content-blocks.property-scaffold-cache.js
+++ src/Perplex.ContentBlocks/App_Plugins/Perplex.ContentBlocks/utils/property/perplex.content-blocks.property-scaffold-cache.js
@@ -14,10 +14,7 @@
 
         function getByKey(key) {
             if (dataTypeKeyToId[key] == null) {
-                dataTypeKeyToId[key] = dataTypeResource.getAll().then(function (dataTypes) {
-                    var dataType = _.find(dataTypes, function (dataType) {
-                        return dataType.key === key;
-                    });
+                dataTypeKeyToId[key] = dataTypeResource.getById(key).then(function (dataType) {
                     if (dataType == null) {
                         throw new Error('No data type found with key "' + key + '"');
                     }
