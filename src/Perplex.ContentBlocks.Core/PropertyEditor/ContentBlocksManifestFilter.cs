15d891db3801ce5defc73cfd6a3ead7e9add4fa1
Daniël Knippers <daniel.knippers@perplex.nl>
2022-11-28 09:20:17 +0100

    Fixes v11 AngularJS module load error
    Umbraco changed the way package.manifest files are loaded in
    897cf4ca195927e7bf9932f740fd25461b68a226 which broke our package since
    the JS file that declares our module is no longer loaded first.
    
    We now ensure the file is loaded first by using an IManifestFilter

---
+++ src/Perplex.ContentBlocks.Core/PropertyEditor/ContentBlocksManifestFilter.cs
@@ -0,0 +1,42 @@
+﻿#if NET5_0_OR_GREATER
+
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Manifest;
+using Umbraco.Extensions;
+
+namespace Perplex.ContentBlocks.PropertyEditor
+{
+    /// <summary>
+    /// Manifest Filter to ensure the main ContentBlocks JavaScript file that creates the AngularJS module "perplexContentBlocks" always runs first.
+    /// Starting with Umbraco v11 the load order of package.manifest files has changed and the file <see cref="MANIFEST_FILE"/> is no longer loaded first like it
+    /// was in v8, v9 and v10.
+    /// This filter is applied to v9 and v10 as well to pre-empt Umbraco backporting the v11 change to v9 and v10, breaking them too.
+    /// </summary>
+    public class ContentBlocksManifestFilter : IManifestFilter
+    {
+        private const string MANIFEST_FILE = "perplex.content-blocks.requires.js";
+
+        public void Filter(List<PackageManifest> manifests)
+        {
+            var requiresManifest = manifests.FirstOrDefault(m => m.Scripts.Any(script => script.InvariantEndsWith(MANIFEST_FILE)));
+
+            if (requiresManifest is not null)
+            {
+                // Ensure the requires file is loaded first
+                manifests.Remove(requiresManifest);
+                manifests.Insert(0, requiresManifest);
+            }
+        }
+    }
+
+    public class ContentBlocksManifestFilterComposer : IComposer
+    {
+        public void Compose(IUmbracoBuilder builder)
+            => builder.ManifestFilters().Append<ContentBlocksManifestFilter>();
+    }
+}
+
+#endif
