16cc5207ce64cd11dffc0a7bf76543960ec62111
Daniël Knippers <daniel.knippers@perplex.nl>
2023-01-13 11:28:00 +0100

    Add (empty) value converter for Perplex.ContentBlocks.BlockEditor

---
+++ src/Perplex.ContentBlocks.Core/PropertyEditor/ContentBlocksBlockEditorValueConverter.cs
@@ -0,0 +1,18 @@
+﻿#if NET5_0_OR_GREATER
+using Umbraco.Cms.Core.Logging;
+using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PropertyEditors.ValueConverters;
+using static Umbraco.Cms.Core.PropertyEditors.BlockListConfiguration;
+
+namespace Perplex.ContentBlocks.PropertyEditor
+{
+    // TODO: Need Umbraco 11 for BlockPropertyValueConverterBase.
+
+    // public class ContentBlocksBlockEditorValueConverter : BlockPropertyValueConverterBase<BlockListModel, BlockListItem, BlockListLayoutItem, BlockConfiguration>
+    // {
+    //     public override bool IsConverter(IPublishedPropertyType propertyType)
+    //         => propertyType.EditorAlias == Constants.PropertyEditor.BlockEditor.Alias;
+    // }
+}
+#endif
