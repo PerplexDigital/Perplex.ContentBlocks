933c91d74b7b15d135101714d61abde4eaf770c2
Daniël Knippers <daniel.knippers@perplex.nl>
2023-09-06 16:16:48 +0200

    Start new solution structure

--- pkg/version.ps1
+++
@@ -1,4 +0,0 @@
-# Emits Perplex.ContentBlocks version obtained from the DLL
-$projectDir = "$PSScriptRoot\..\src\Perplex.ContentBlocks.Core"
-$dll = Get-Item "$projectDir\bin\Release\*\Perplex.ContentBlocks.dll" | Select-Object -First 1
-Write-Output $dll | % { $_.VersionInfo.ProductVersion }
