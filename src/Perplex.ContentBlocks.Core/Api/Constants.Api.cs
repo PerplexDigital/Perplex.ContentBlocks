fc16f1a9977c4c42f7b1ab7df62ac6b93dc9cbe4
Daniël Knippers <daniel.knippers@perplex.nl>
2024-06-28 11:44:28 +0200

    Add ability to pass Bearer token in querystring for our APIs

--- src/Perplex.ContentBlocks.Core/Api/Constants.Api.cs
+++ src/Perplex.ContentBlocks.Core/Api/Constants.Api.cs
@@ -5,5 +5,7 @@ public static partial class Constants
     public static partial class Api
     {
         public const string ApiName = "Perplex.ContentBlocks";
+        public const string BaseRoute = "perplex-content-blocks/api";
+        public const string TokenQueryStringKey = "token";
     }
 }
---
+++ src/Perplex.ContentBlocks.Core/Api/ContentBlocksApiComposer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Perplex.ContentBlocks.Api;
+public class ContentBlocksApiComposer : IComposer
+{
+    public void Compose(IUmbracoBuilder builder)
+    {
+        builder.Services.AddSingleton<IStartupFilter, ContentBlocksApiStartupFilter>();
+    }
+}
