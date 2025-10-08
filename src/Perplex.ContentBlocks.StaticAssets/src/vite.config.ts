ae770c50278675efa449ba320666b86b69c9cd23
Daniël Knippers <daniel.knippers@perplex.nl>
2025-10-08 13:58:31 +0200

    Simplify static web asset structure and add cache buster to perplex.content-blocks.js

---
+++ src/Perplex.ContentBlocks.StaticAssets/src/vite.config.ts
@@ -0,0 +1,32 @@
+import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+import addContentBlocksVersion from './add-content-blocks-version.ts';
+
+export default defineConfig({
+    build: {
+        lib: {
+            entry: 'src/index.ts',
+            formats: ['es'],
+        },
+        outDir: '../wwwroot/App_Plugins/Perplex.ContentBlocks/',
+        emptyOutDir: true,
+        sourcemap: true,
+        rollupOptions: {
+            external: [/^@umbraco/],
+        },
+    },
+    define: {
+        'process.env': {},
+    },
+    plugins: [
+        viteStaticCopy({
+            targets: [
+                {
+                    src: 'umbraco-package.json',
+                    dest: '.',
+                },
+            ],
+        }),
+        addContentBlocksVersion(),
+    ],
+});
